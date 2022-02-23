ece055ed1c9acac046355d13310b524ea701dcab
Dayoung <dayoungkim9105@gmail.com>
2022-02-23 19:47:00 +0900

    project setting

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+
+  html {
+    width:100%;
+    height:100%;
+  }
+
+  body {
+    width: 100%;
+    height: 100%;
+    color: #424242;
+    font-family: 'Pretendard', sans-serif;
+    font-weight: 300;
+    overflow: hidden;
+    padding: 0;
+    margin: 0;
+    font-size: 18px;
+    transition: all ease 0.7s 0s;
+  }
+
+  *, button, input, select, label {
+    font-family: 'Pretendard', sans-serif;
+    box-sizing: border-box;
+  }
+
+`;
+
+export default GlobalStyle;
